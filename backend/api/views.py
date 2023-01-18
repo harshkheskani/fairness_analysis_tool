c61c0b49a91f2a707f0e491afd076ef558328d62
Harsh Kheskani <hkheskani01@gmail.com>
2023-01-18 12:25:03 +0000

    Setup djangorestframework

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes (request):
-    return JsonResponse('Our API', safe = False)
+    return Response('Our API' )
