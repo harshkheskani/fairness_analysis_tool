67a1f46019487d7041ff2133abb16bad019d17d5
Harsh Kheskani <hkheskani01@gmail.com>
2023-03-04 17:43:13 +0000

    Update dynamic Y-axis Label

--- frontend/src/components/BarChart.jsx
+++ frontend/src/components/BarChart.jsx
@@ -14,7 +14,11 @@ const BarChart = ({ continentCount, locations, searchResults }) => {
     { name: "Europe", value: 1289747, percentage: 21.26 },
     { name: "Northern America", value: 1134091, percentage: 18.69 },
     { name: "Asia", value: 600711, percentage: 9.9 },
-    { name: "Latin America and the Caribbean", value: 185393, percentage: 3.06 },
+    {
+      name: "Latin America and the Caribbean",
+      value: 185393,
+      percentage: 3.06,
+    },
     { name: "Oceania", value: 157943, percentage: 2.6 },
     { name: "Africa", value: 131603, percentage: 2.17 },
     { name: "Antarctica", value: 9626, percentage: 0.16 },
@@ -31,16 +35,16 @@ const BarChart = ({ continentCount, locations, searchResults }) => {
     const rankRanges = [];
     if (locations && Object.keys(locations).length > 0) {
       Object.entries(locations).forEach(([continent, data]) => {
-        const ranks = data.map((obj) => ({name: obj.name, value: obj.rank}));
+        const ranks = data.map((obj) => ({ name: obj.name, value: obj.rank }));
         if (ranks.length > 0) {
           const sum = ranks.reduce((total, rank) => total + rank.value, 0);
           const average = sum / ranks.length;
-          continentAverages.push({name: continent, value: average});
-          const low = Math.min(...ranks.map(rank => rank.value));
-          const high = Math.max(...ranks.map(rank => rank.value));
-          rankRanges.push({name: continent, low, high});
+          continentAverages.push({ name: continent, value: average });
+          const low = Math.min(...ranks.map((rank) => rank.value));
+          const high = Math.max(...ranks.map((rank) => rank.value));
+          rankRanges.push({ name: continent, low, high });
         }
-        continentRanks.push({name: continent, value: ranks});
+        continentRanks.push({ name: continent, value: ranks });
       });
       setRanksPercontinent(continentRanks);
       setAvgContinentRank(continentAverages);
@@ -58,12 +62,12 @@ const BarChart = ({ continentCount, locations, searchResults }) => {
     const skewness =
       data.reduce((acc, val) => acc + Math.pow(val - mean, 3), 0) /
       (n * Math.pow(stdDev, 3));
-    return skewness
+    return skewness;
   }
 
   const [barChartData, setBarChartData] = useState(initialData);
-  const [searchDataDistribution, setSearchDataDistribution] = useState()
-  const [expectedExposure, setExpectedExposure] = useState()
+  const [searchDataDistribution, setSearchDataDistribution] = useState();
+  const [expectedExposure, setExpectedExposure] = useState();
 
   function getPercentageByName(name) {
     const region = initialData.find((region) => region.name === name);
@@ -71,17 +75,17 @@ const BarChart = ({ continentCount, locations, searchResults }) => {
   }
 
   function updatingContinentCount() {
-    const searchDistribution = []
+    const searchDistribution = [];
     const formattedMapData = [];
     const listExpectedExposure = [];
     const valueSum = Object.values(continentCount).reduce((a, b) => a + b, 0);
     for (let key in continentCount) {
       if (continentCount.hasOwnProperty(key)) {
         const searchRatio = continentCount[key] / valueSum;
-        searchDistribution.push({name: key, value: searchRatio*100})
+        searchDistribution.push({ name: key, value: searchRatio * 100 });
         const fullDataSetRatio = getPercentageByName(key) / 100;
         const expectedExposure = Math.abs(searchRatio - fullDataSetRatio) * 100;
-        listExpectedExposure.push({name: key, value: expectedExposure});
+        listExpectedExposure.push({ name: key, value: expectedExposure });
         // create a new object with the desired key names
         let newObj = {
           name: key,
@@ -93,8 +97,8 @@ const BarChart = ({ continentCount, locations, searchResults }) => {
       }
     }
     setBarChartData(searchDistribution);
-    setSearchDataDistribution(searchDistribution)
-    setExpectedExposure(listExpectedExposure)
+    setSearchDataDistribution(searchDistribution);
+    setExpectedExposure(listExpectedExposure);
   }
 
   // updating map data
@@ -104,6 +108,7 @@ const BarChart = ({ continentCount, locations, searchResults }) => {
     }
   }, [continentCount]);
 
+  const [yAxisLabel, setYAxisLabel] = useState()
 
   return (
     <Box>
@@ -116,18 +121,35 @@ const BarChart = ({ continentCount, locations, searchResults }) => {
           marginTop: "10px",
         }}
       >
-        <ToggleButtonGroup
-          exclusive
-
-          aria-label="text alignment"
-        >
-          <ToggleButton value="center" aria-label="centered" onClick={() => {setBarChartData(searchDataDistribution)}}>
+        <ToggleButtonGroup exclusive aria-label="text alignment">
+          <ToggleButton
+            value="center"
+            aria-label="centered"
+            onClick={() => {
+              setBarChartData(searchDataDistribution);
+              setYAxisLabel("Distribution (%)")
+            }}
+          >
             <Typography>Distribution of Search Results</Typography>
           </ToggleButton>
-          <ToggleButton value="center" aria-label="centered" onClick={() => {setBarChartData(expectedExposure)}} >
+          <ToggleButton
+            value="center"
+            aria-label="centered"
+            onClick={() => {
+              setBarChartData(expectedExposure);
+              setYAxisLabel("Expected Exposure (%)")
+            }}
+          >
             <Typography>Expected Exposure</Typography>
           </ToggleButton>
-          <ToggleButton value="left" aria-label="left aligned" onClick={() => {setBarChartData(avgContinentRank)}}>
+          <ToggleButton
+            value="left"
+            aria-label="left aligned"
+            onClick={() => {
+              setBarChartData(avgContinentRank);
+              setYAxisLabel("Average Rank")
+            }}
+          >
             <Typography>Average Rank</Typography>
           </ToggleButton>
           <ToggleButton value="center" aria-label="centered">
@@ -141,13 +163,23 @@ const BarChart = ({ continentCount, locations, searchResults }) => {
           data={barChartData}
           // zkeys={["Africa","Asia","unknown","Europe", "Latin America and the Caribbean", "Antarctica", "Northern America","Oceania"]}
           indexBy="name"
-          margin={{ top: 50, right: 50, bottom: 50, left: 50 }}
+          margin={{ top: 50, right: 50, bottom: 50, left: 100 }}
           padding={0.3}
           colors={{ scheme: "nivo" }}
           labelTextColor={{ from: "color", modifiers: [["darker", 1.6]] }}
           animate={true}
           motionStiffness={90}
           motionDamping={15}
+          axisLeft={{
+            legend: `${yAxisLabel}`,
+            legendPosition: "middle",
+            legendOffset: -60,
+          }}
+          axisBottom={{
+            legend: "Contient",
+            legendPosition: "middle",
+            legendOffset: 35,
+          }}
         />
       </Box>
     </Box>
